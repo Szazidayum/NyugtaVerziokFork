d09e7ca8a6c60ede67a70bdf8aece1a877793f5d
Szazidayum <szabrinamolnar@gmail.com>
2022-03-18 12:21:54 +0100

    2. próbálozás: Nyugta1 fájl static változók létrehozása

--- src/nyugtaprojektek/Nyugta1.java
+++ src/nyugtaprojektek/Nyugta1.java
@@ -1,32 +1,36 @@
 package nyugtaprojektek;
 
 public class Nyugta1 {
+    static String csillagok = "******************";
+    static String vonal = "------------------";
+    static String duplaVonal = "==================";
+    static String kisVonal = "_______";
 
     public static void main(String[] args) {
-        System.out.println("******************");
+        System.out.println(csillagok);
         System.out.println("     Nyugta 1");
-        System.out.println("******************");
+        System.out.println(csillagok);
         System.out.println("Tétel 1:    350 Ft");
         System.out.println("Tétel 2:     90 Ft");
         System.out.println("Tétel 3:    650 Ft");
-        System.out.println("==================");
+        System.out.println(duplaVonal);
         System.out.println("Összesen:  1090 Ft");
-        System.out.println("------------------");
+        System.out.println(vonal);
         System.out.println("Kedvezmény: 109 Ft");
         System.out.println("(10%)");
-        System.out.println("==================");
+        System.out.println(duplaVonal);
         System.out.println("Fizetendő:  981 Ft");
-        System.out.println("------------------");
+        System.out.println(vonal);
         System.out.println("");
-        System.out.print("_______");
+        System.out.print(kisVonal);
         System.out.print("   ");
-        System.out.println("_______");
+        System.out.println(kisVonal);
         System.out.print(" Dátum");
         System.out.print("   ");
         System.out.println("   Név");
-        System.out.println("******************");
+        System.out.println(csillagok);
         System.out.println("        CÉG");
-        System.out.println("******************");
+        System.out.println(csillagok);
     }
     
 }
